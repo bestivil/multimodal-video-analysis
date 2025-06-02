1ee490b5234cbb2ddb856ab9be2dcc68f1fd674a
bruce <psybe1@nottingham.ac.uk>
2025-06-02 02:58:31 +0100

    chore: added youtube url validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,24 +17,32 @@ export default function Home() {
     setSeekTimestamp(null);
   }, [submittedURL]);
 
+  const validURL = submittedURL.includes("youtube.com");
+
   return (
     <QueryClientProvider client={queryClient}>
       <div className="flex flex-col p-16 gap-12 justify-items-center items-center font-[family-name:var(--font-geist-sans)]">
         <VideoURLInput setSubmittedURL={setSubmittedURL} />
-        <div className="flex flex-col md:flex-row gap-2 gap-y-4 justify-around">
-          <EmbedVideo
-            url={submittedURL}
-            onTimeUpdate={setTimestamp}
-            seekTimestamp={seekTimestamp}
-          />
-          <div className="w-px h-96 bg-gray-300 mx-6 hidden md:block" />
-          <Tabs
-            submittedURL={submittedURL}
-            timestamp={timestamp}
-            setTimestamp={setTimestamp}
-            setSeekTimestamp={setSeekTimestamp}
-          />
-        </div>
+        {validURL ? (
+          <div className="flex flex-col md:flex-row gap-2 gap-y-4 justify-around">
+            <EmbedVideo
+              url={submittedURL}
+              onTimeUpdate={setTimestamp}
+              seekTimestamp={seekTimestamp}
+            />
+            <div className="w-px h-96 bg-gray-300 mx-6 hidden md:block" />
+            <Tabs
+              submittedURL={submittedURL}
+              timestamp={timestamp}
+              setTimestamp={setTimestamp}
+              setSeekTimestamp={setSeekTimestamp}
+            />
+          </div>
+        ) : (
+          <div className="flex border-gray-200 text-red-400 mb-4 gap-2 justify-around">
+            <h4>Please enter a valid YouTube URL</h4>
+          </div>
+        )}
       </div>
     </QueryClientProvider>
   );
